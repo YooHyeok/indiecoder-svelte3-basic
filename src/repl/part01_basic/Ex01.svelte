106c3cb9f69c4ed0abaef06223c05f432f875ba1
유재혁 <yjk7454@naver.com>
2025-11-26 19:20:34 +0900

    feat: `Part01_Svelte 기본문법`: Ex01 - 상태값 state와 dom 이벤트리스너 바인딩 예제

---
+++ src/repl/part01_basic/Ex01.svelte
@@ -0,0 +1,13 @@
+<script>
+  let count = 0;
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<div>
+  <div><h2>Ex01</h2></div>
+  <button on:click={handleClick}>
+    클릭수 {count}
+  </button>
+</div>
