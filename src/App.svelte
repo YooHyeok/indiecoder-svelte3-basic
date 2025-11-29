09a39e39711d947b66309557d20198673202e571
유재혁 <yjk7454@naver.com>
2025-11-29 21:33:16 +0900

    feat: Main 컴포넌트 생성 및 Ex01~03 컴포넌트 등록 / App컴포넌트에 Main 컴포넌트 등록

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Main from "./repl/Main.svelte";
 </script>
 
 <main>
@@ -17,15 +18,20 @@
 
   <div class="card">
     <Counter />
+    <br />
+    <hr />
+    <Main />
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
---
+++ src/repl/Main.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Ex01 from "./part01_basic/Ex01.svelte";
+  import Ex02 from "./part01_basic/Ex02.svelte";
+  import Ex03 from "./part01_basic/Ex03.svelte";
+</script>
+
+<div>
+  <div><h1>인프런 Indie Coder</h1></div>
+  <Ex01 />
+  <Ex02 />
+  <Ex03 />
+</div>
