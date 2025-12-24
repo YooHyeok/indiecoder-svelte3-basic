5c5213fd0d4d1d2f1b68ce0f3d4c8b33a85e1ae2
유재혁 <yjk7454@naver.com>
2025-12-24 09:09:27 +0900

    feat: `Part03_Slot `: Ex01 - Slot 기본 예제

---
+++ src/repl/part03_slot/ex01/Slot.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Card from "./Card.svelte";
+</script>
+
+<div>
+  <div><h1>Part03_Ex01</h1></div>
+  <Card>
+    <h2>안녕하세요!</h2>
+    <p>이곳은 내용이 들어가는 영역입니다.</p>
+  </Card>
+  <br />
+  <Card>
+    <h2>Hello Svelte</h2>
+    <p>Svelte를 배우는 여러분을 환영합니다.</p>
+    <p>즐거운 코딩 되세요...</p>
+  </Card>
+</div>
