9608920e9827ff92cd952a09284ec9f0a5e0444a
유재혁 <yjk7454@naver.com>
2025-12-24 09:19:11 +0900

    feat: `Part03_Slot `: Ex02 - Slot alias별칭(name, slot 속성) 예제

---
+++ src/repl/part03_slot/ex02/Slot.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Card from "./Card.svelte";
+</script>
+
+<div>
+  <div><h1>Part03_Ex02</h1></div>
+  <Card>
+    <!-- slot의 name속성에 부여한 별칭과 매핑 -->
+    <span slot="name"> 홍길동 </span>
+    <span slot="address">
+      서울특별시 <br />
+      여의도동
+    </span>
+  </Card>
+</div>
