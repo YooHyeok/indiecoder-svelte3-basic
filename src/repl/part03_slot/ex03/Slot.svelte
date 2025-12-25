fcaa2e370a0bc5ebce233d173ea668b30400f8be
유재혁 <yjk7454@naver.com>
2025-12-25 19:23:22 +0900

    feat: `Part03_Slot `: Ex03 - Slot alias name속성 기본출력 예제

---
+++ src/repl/part03_slot/ex03/Slot.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Card from "./Card.svelte";
+</script>
+
+<div>
+  <div><h1>Part03_Ex03</h1></div>
+  <Card>
+    <!-- <span slot="name"> 홍길동 </span> -->
+    <span slot="address">
+      서울특별시 <br />
+      여의도동
+    </span>
+  </Card>
+</div>
