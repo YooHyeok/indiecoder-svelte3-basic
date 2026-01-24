8b50b49efecd9767c1afd7c5bb17b8c407fc677b
유재혁 <yjk7454@naver.com>
2026-01-25 00:20:42 +0900

    feat: `Part03_Slot `: Ex05 - 번외: 동적 class 적용

---
+++ src/repl/part03_slot/ex05/Slot.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Card from "./Card.svelte";
+</script>
+
+<div>
+  <div><h1>Part03_Ex05</h1></div>
+  <Card>
+    <span slot="name"> 홍길동 </span>
+    <span slot="address">
+      서울특별시 <br />
+      여의도동
+    </span>
+    <span slot="email"> webdevyoo@gmail.com </span>
+  </Card>
+</div>
