54377cdf2ad02774384f03cd9492a3be3f84a0a2
유재혁 <yjk7454@naver.com>
2026-01-31 23:35:19 +0900

    feat: `Part03_Slot `: Ex06 - 데이터 전달 slot props(scoped slot) 예제

---
+++ src/repl/part03_slot/ex06/Card.svelte
@@ -0,0 +1,72 @@
+<script>
+  let hovering = false;
+  const enter = () => (hovering = true);
+  const leave = () => (hovering = false);
+</script>
+
+<article
+  class="contact-card"
+  class:hovering
+  on:mouseenter={enter}
+  on:mouseleave={leave}
+>
+  <h2>
+    <slot name="name">
+      <!-- slot="{name}"의 name과 매핑되지 않을 경우 출력될 기본값 -->
+      <span class="missing">이름 미입력</span>
+    </slot>
+  </h2>
+  <div class="address">
+    <slot name="address">
+      <span class="missing">주소 미입력</span>
+    </slot>
+  </div>
+  {#if $$slots.email}
+    <!-- email 이름의 slot이 존재할경우 렌더링 -->
+    <div class="email">
+      <hr />
+      <slot {hovering} name="email">
+        <span class="missing">이메일 미입력</span>
+      </slot>
+    </div>
+  {/if}
+</article>
+
+<style>
+  .contact-card {
+    width: 300px;
+    border: 1px solid #aaa;
+    border-radius: 2px;
+    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.1);
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h2 {
+    padding: 0 0 0.2em 0;
+    margin: 0 0 1em 0;
+    border-bottom: 1px solid #ff3e00;
+  }
+
+  .address,
+  .email {
+    padding: 0 0 0 1.5em;
+    background: 0 50% no-repeat;
+    background-size: 1em 1em;
+    margin: 0 0 0.5em 0;
+    line-height: 1.2;
+  }
+  .address {
+    background-image: url(tutorial/icons/map-marker.svg);
+  }
+  .email {
+    background-image: url(tutorial/icons/emain.svg);
+  }
+  .missing {
+    color: #999;
+  }
+
+  .hovering {
+    background-color: violet;
+  }
+</style>
---
+++ src/repl/part03_slot/ex06/Slot.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Card from "./Card.svelte";
+</script>
+
+<div>
+  <div><h1>Part03_Ex06</h1></div>
+  <!-- Card컴포넌트에서 slot태그를 통해 전달한 hovering 값 수신 -->
+  <Card let:hovering>
+    <span slot="name"> 홍길동 </span>
+    <span slot="address">
+      서울특별시 <br />
+      여의도동
+    </span>
+    <span slot="email">
+      {#if hovering}
+        <h2><b>webdevyoo@gmail.com</b></h2>
+      {:else}
+        webdevyoo@gmail.com
+      {/if}
+    </span>
+  </Card>
+</div>
