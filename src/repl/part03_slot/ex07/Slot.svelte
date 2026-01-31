6f8f6ca12ced919810a2853a6bd805fe7cba344d
유재혁 <yjk7454@naver.com>
2026-02-01 00:19:45 +0900

    feat: `Part03_Slot `: Ex07 - fragment(svelte:fragment)예제

---
+++ src/repl/part03_slot/ex07/Slot.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Widget from "./Widget.svelte";
+</script>
+
+<div>
+  <div><h1>Part03_Ex07</h1></div>
+  <Widget>
+    <h2 slot="header">Hello</h2>
+    <!-- <div slot="footer">
+      <p>All rights reserved.</p>
+      <p>Copyright (c) 2019 Svelte Industries</p>
+    </div> -->
+    <!-- `svelte:fragment` : 불필요한 DOM 요소를 만들지 않고 슬롯을 사용 -->
+    <svelte:fragment slot="footer">
+      <p>All rights reserved.</p>
+      <p>Copyright (c) 2019 Svelte Industries</p>
+    </svelte:fragment>
+  </Widget>
+</div>
---
+++ src/repl/part03_slot/ex07/Widget.svelte
@@ -0,0 +1,16 @@
+<article class="contact-card">
+  <slot name="header">No header was provided</slot>
+  <p>Some content between header and footer</p>
+  <slot name="footer">No header was provided</slot>
+</article>
+
+<style>
+  .contact-card {
+    width: 300px;
+    border: 1px solid #aaa;
+    border-radius: 2px;
+    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.1);
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
