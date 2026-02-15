4c967b3e1a31f45cd29cd1c6cda8f8adb4a85d6f
유재혁 <yjk7454@naver.com>
2026-02-15 12:53:28 +0900

    feat: `Part04_transition `: Transition 실습 예제

---
+++ src/repl/part04_transition/Transition01.svelte
@@ -0,0 +1,81 @@
+<script>
+  import { fade, blur, fly, slide, scale, draw } from "svelte/transition";
+
+  let current = null;
+  let visibles = {
+    fade: false,
+    slide: false,
+    blur: false,
+    fly: false,
+    scale: false,
+    inOut: false,
+  };
+
+  const changeVisible = (type) => {
+    visibles[current] = false;
+    if (current == type) {
+      current = null;
+      return;
+    }
+    current = type;
+    visibles[type] = !visibles[type];
+  };
+</script>
+
+<div>
+  <div><h1>Part04_Ex01</h1></div>
+  <article class="contact-card">
+    <button on:click={() => changeVisible("fade")}>fade</button>
+    <button on:click={() => changeVisible("slide")}>slide</button>
+    <button on:click={() => changeVisible("blur")}>blur</button>
+    <button on:click={() => changeVisible("fly")}>fly</button>
+    <button on:click={() => changeVisible("scale")}>scale</button>
+    <button on:click={() => changeVisible("inOut")}>in,out</button>
+
+    <br />
+
+    {#if visibles.fade}
+      <div transition:fade class="wrap">
+        <h1>fade 예제</h1>
+      </div>
+    {/if}
+
+    {#if visibles.slide}
+      <div transition:slide class="wrap">
+        <h1>slide 예제</h1>
+      </div>
+    {/if}
+
+    {#if visibles.blur}
+      <div transition:blur class="wrap">
+        <h1>blur 예제</h1>
+      </div>
+    {/if}
+
+    {#if visibles.fly}
+      <div transition:fly class="wrap">
+        <h1>fly 예제</h1>
+      </div>
+    {/if}
+
+    {#if visibles.scale}
+      <div transition:scale class="wrap">
+        <h1>scale 예제</h1>
+      </div>
+    {/if}
+
+    {#if visibles.inOut}
+      <div in:fade out:slide class="wrap">
+        <h1>in, out 예제</h1>
+      </div>
+    {/if}
+  </article>
+</div>
+
+<style>
+  .wrap {
+    padding: 20px;
+    background-color: #288edd;
+    text-align: center;
+  }
+</style>
